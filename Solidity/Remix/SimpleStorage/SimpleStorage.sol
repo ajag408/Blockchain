ce803bb406adea21443e2dbabe45fe7934c62838
Akash Jag <akashjagannathan408@gmail.com>
2022-09-22 02:12:38 -0700

    solidity types

---
+++ Solidity/Remix/SimpleStorage/SimpleStorage.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.8; // ^ = any version and above
+
+contract SimpleStorage {
+    //boolean, uint (whole number, +), int (whole number, + or -),
+    // address, bytes
+    // bool hasFavoriteNumber = true;
+    // string favoriteNumberInText = "Five";
+    // int256 favoriteInt = -5;
+    // address myAddress = 0x95087DFd490c67eb1E5b309531AC032E19B244b3;
+    // bytes32 favoriteBytes = "cat"; 
+
+    //initialized to 0
+    uint256 favoriteNumber;
+}
--- Solidity/Remix/contracts/SimpleStorage.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.8; // ^ = any version and above
-
-contract SimpleStorage {
-
-}
