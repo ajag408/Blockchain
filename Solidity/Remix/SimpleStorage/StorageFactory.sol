d33db3b0b32677dc06797fa672f0f5852872353a
Akash Jag <akashjagannathan408@gmail.com>
2022-09-27 01:31:03 -0700

    factory contract and virtual override

--- Solidity/Remix/SimpleStorage/StorageFactory.sol
+++ Solidity/Remix/SimpleStorage/StorageFactory.sol
@@ -12,7 +12,16 @@ contract StorageFactory{
         simpleStorageArray.push(simpleStorage);
     }
 
-    function sfStore(uint256 _simpleStorageIndex, uint _SimpleStorageNumber) public {
-        
+    function sfStore(uint256 _simpleStorageIndex, uint _simpleStorageNumber) public {
+        //to interact w contract need
+        //1) address
+        //2) ABI
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        return simpleStorage.retrieve();
     }
 }
