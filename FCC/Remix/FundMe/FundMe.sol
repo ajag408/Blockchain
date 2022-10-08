c59c29174ebb1e78f5104c559c58c015c9e7c945
Akash Jag <akashjagannathan408@gmail.com>
2022-10-08 01:48:44 -0700

    interfaces, libraries

--- FCC/Remix/FundMe/FundMe.sol
+++ FCC/Remix/FundMe/FundMe.sol
@@ -1,21 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-contract FundMe {
+import "./PriceConverter.sol";
 
-    uint256 public minimumUsd = 50;
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public minimumUsd = 50 * 1e18;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
 
     function fund() public payable{
-        require(msg.value >= minimumUsd, "Didn't send enough"); //1e18 wei = 1 Eth
+        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough"); //1e18 wei = 1 Eth
+        //18 decimals
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
     }
 
-    function getPrice() public {
-        //ABI
-        //Address (Goerli, Eth/usd): 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+
+
+    function withdraw() public {
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        //reset array
+        //withdraw funds
     }
 
-    function getConversionRate() public {}
-
-    // function withdraw(){}
-    //blahblah
 }
---
+++ FCC/Remix/FundMe/PriceConverter.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    function getPrice() internal view returns(uint256) {
+        //ABI
+        //Address (Goerli, Eth/usd): 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        //ETH in terms of USD
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice*ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+}
