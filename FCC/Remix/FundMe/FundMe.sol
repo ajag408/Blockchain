fe1c4ae5cf823094ad4ea6871c9e553062129453
Akash Jag <akashjagannathan408@gmail.com>
2022-10-10 17:42:07 -0700

    fund me withdraw to owner, receive, fallback

--- FCC/Remix/FundMe/FundMe.sol
+++ FCC/Remix/FundMe/FundMe.sol
@@ -3,16 +3,27 @@ pragma solidity ^0.8.8;
 
 import "./PriceConverter.sol";
 
+//837,285 to create w/o ops
+//optimize with constant, immutable
+
+error NotOwner();
+
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 50 * 1e18;
+    uint256 public constant MINIMUM_USD = 50 * 1e18;
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
+    address public immutable i_owner;
+
+    constructor(){
+        i_owner = msg.sender;    
+    }
+
     function fund() public payable{
-        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough"); //1e18 wei = 1 Eth
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough"); //1e18 wei = 1 Eth
         //18 decimals
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
@@ -20,13 +31,45 @@ contract FundMe {
 
 
 
-    function withdraw() public {
+    function withdraw() public onlyOwner {
         for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
         //reset array
+        funders = new address[](0);
+
         //withdraw funds
+        // //transfer
+        //     //msg.sender type = address
+        //     // payable(msg.sender) type = payable
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // //send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+
+        //call
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
     }
 
+    modifier onlyOwner{
+        // require(msg.sender == i_owner, "Sender is not owner!");
+        if(msg.sender != i_owner){
+            revert NotOwner();
+        }
+        _;
+    }
+
+    //What happens when someone sends funds to contract w/o using fund method
+
+    //receive, fund methods
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
 }
