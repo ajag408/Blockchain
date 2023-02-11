ed02a2eb0e5cee4fdb023f9a686ca8316e1914a4
Akash Jag <akashjagannathan408@gmail.com>
2023-02-11 03:45:59 -0800

    get latest eth block w info and transactions + receipts

---
+++ Alchemy/EthDev/blockexplorer/src/components/Main.components.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
+import Home from "./home.component";
+
+const Main = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      {/* <Route exact path="/signup-student" component={CreateStudent} />
+    <Route exact path="/signup-company" component={CreateCompany} />
+    <Route exact path="/company-signin" component={CompanySignin} />
+    <Route exact path="/student-signin" component={StudentSignin} />
+    <Route exact path="/company/landing" component={CompanyJobPosting} />
+    <Route exact path="/company/profile" component={CompanyProfile} />
+    <Route exact path="/company/students" component={CompanyStudentsTab} />
+    <Route exact path="/company/events" component={MakeEvents} />
+    <Route exact path="/student/landing" component={JobSearch} />
+    <Route exact path="/student/profile" component={StudentProfile} />
+    <Route exact path="/student/applications" component={Applications} />
+    <Route exact path="/student/events" component={ViewEvents} />
+    <Route exact path="/student/students" component={StudentSearch} />
+    <Route exact path="/company/student/:id" component={ViewStudentProfileFromCompany} />
+    <Route exact path="/student/:id" component={ViewStudentProfileFromStudent} />
+    <Route exact path="/company/:id" component={ViewCompanyProfile} />
+    */}
+      <Route render={() => <Redirect to={{ pathname: "/" }} />} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Main;
