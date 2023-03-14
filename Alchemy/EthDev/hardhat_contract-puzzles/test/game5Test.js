c40527ea4922033a1f0d726be5eed42b2bee933a
Akash Jag <akashjagannathan408@gmail.com>
2023-03-14 00:55:19 -0700

    completed challenge 5

--- Alchemy/EthDev/hardhat_contract-puzzles/test/game5Test.js
+++ Alchemy/EthDev/hardhat_contract-puzzles/test/game5Test.js
@@ -1,21 +1,39 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
+const { ethers } = require("hardhat");
 
-describe('Game5', function () {
+describe("Game5", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game5');
+    const Game = await ethers.getContractFactory("Game5");
     const game = await Game.deploy();
 
-    return { game };
+    var wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+    const threshold = "0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf";
+
+    while (parseInt(wallet.address) >= parseInt(threshold)) {
+      wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+    }
+
+    const signer0 = ethers.provider.getSigner(0);
+    // console.log("signer balance: ", await signer0.getBalance());
+
+    await signer0.sendTransaction({
+      to: wallet.address,
+      value: ethers.utils.parseEther("1"),
+    });
+
+    // console.log("wallet balance: ", await wallet.getBalance());
+
+    return { game, wallet };
   }
-  it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+  it("should be a winner", async function () {
+    const { game, wallet } = await loadFixture(deployContractAndSetVariables);
 
     // good luck
 
-    await game.win();
+    await game.connect(wallet).win();
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
