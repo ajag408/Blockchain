344ccb63899fad15215a93ae0696fd2059896fc1
Akash Jag <akashjagannathan408@gmail.com>
2023-05-06 08:54:50 -0700

    still not able to execute

--- Alchemy/EthDev/hardhat_gov/scripts/execute.js
+++ Alchemy/EthDev/hardhat_gov/scripts/execute.js
@@ -2,15 +2,19 @@ const { ethers } = require("hardhat");
 
 const tokenAddr = "0xdA87E876B7eE28291C4E2E1b8Fed04CCA0686379";
 const govAddr = "0x049CF58FF52f35194F2bBE4542426a7f165A4931";
-const owner = "0x7210F409037e0071c8AeD2B8991f597b19431564";
 
 async function main() {
+  const url = process.env.ALCHEMY_SEPOLIA_URL;
+  let privateKey = process.env.SEPOLIA_PRIVATE_KEY;
+  const provider = new ethers.providers.JsonRpcProvider(url);
+  let owner = new ethers.Wallet(privateKey, provider);
+
   // attach to the deployed gov token
   const token = await hre.ethers.getContractAt("MyToken", tokenAddr);
 
   const governor = await hre.ethers.getContractAt("MyGovernor", govAddr);
 
-  const beforebalance = await token.balanceOf(owner);
+  const beforebalance = await token.balanceOf(owner.address);
 
   console.log(
     "My balance of the gov token before executing the vote: ",
@@ -18,7 +22,7 @@ async function main() {
   );
 
   const proposalId = ethers.BigNumber.from(
-    "18815366083867697168662531599589102093169529522873446219692412326029904685416"
+    "55464478978233079024076953986344115705055625782618195002781782344501279211817"
   );
 
   const state = await governor.state(proposalId);
@@ -27,10 +31,10 @@ async function main() {
   //   console.log(typeof ethers.utils.parseEther("25000"));
   const tx = await governor.execute(
     [token.address],
-    [6],
+    [9],
     [
       token.interface.encodeFunctionData("mint", [
-        owner,
+        owner.address,
         ethers.utils.parseEther("25000"),
       ]),
     ],
@@ -45,7 +49,7 @@ async function main() {
   const receipt = await tx.wait();
   const event = receipt.events.find((x) => x.event === "ProposalExecuted");
   console.log("event: ", event);
-  const afterbalance = await token.balanceOf(owner);
+  const afterbalance = await token.balanceOf(owner.address);
 
   console.log(
     "My balance of the gov token AFTER executing the vote: ",
--- Alchemy/EthDev/hardhat_gov/scripts/propVE.js
+++ Alchemy/EthDev/hardhat_gov/scripts/propVE.js
@@ -1,6 +1,8 @@
+const { ethers } = require("hardhat");
+
 const tokenAddr = "0xdA87E876B7eE28291C4E2E1b8Fed04CCA0686379";
 const govAddr = "0x049CF58FF52f35194F2bBE4542426a7f165A4931";
-//proposal counter: next one at [7]
+
 async function main() {
   // attach to the deployed gov token
   const token = await hre.ethers.getContractAt("MyToken", tokenAddr);
@@ -15,7 +17,7 @@ async function main() {
   //propose to mint extra 25K tokens to owner
   const tx = await governor.propose(
     [token.address],
-    [9],
+    [11],
     [
       token.interface.encodeFunctionData("mint", [
         owner.address,
@@ -24,21 +26,42 @@ async function main() {
     ],
     "Give the owner more tokens!"
   );
-  const receipt = await tx.wait();
+  const receipt = await tx.wait(2);
   const event = receipt.events.find((x) => x.event === "ProposalCreated");
   const { proposalId } = event.args;
 
-  console.log("Proposal ID: ", proposalId);
-
-  //   const url = process.env.ALCHEMY_SEPOLIA_URL;
-
-  //   const provider = new ethers.providers.JsonRpcProvider(url);
-
-  //   // wait for the block voting delay
-  //   await hre.network.provider.request("evm_mine");
-
   const state = await governor.state(proposalId);
   console.log("State of newly created proposal: ", state);
+
+  const tx2 = await governor.castVote(proposalId, 1);
+  const receipt2 = await tx2.wait(10);
+  const voteCastEvent = receipt2.events.find((x) => x.event === "VoteCast");
+
+  console.log("Who cast the vote?: ", voteCastEvent.args.voter);
+
+  const tx3 = await governor.execute(
+    [token.address],
+    [11],
+    [
+      token.interface.encodeFunctionData("mint", [
+        owner.address,
+        ethers.utils.parseEther("25000"),
+      ]),
+    ],
+    ethers.utils.keccak256(
+      ethers.utils.toUtf8Bytes("Give the owner more tokens!")
+    )
+    // {
+    //   gasLimit: 100000,
+    // }
+  );
+
+  const afterbalance = await token.balanceOf(owner.address);
+
+  console.log(
+    "My balance of the gov token AFTER executing the vote: ",
+    ethers.utils.formatEther(afterbalance)
+  );
 }
 
 main()
