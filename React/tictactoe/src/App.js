b5e20e180831a78c9605917a3fd7cf6321c2504b
Akash Jag <akashjagannathan408@gmail.com>
2023-07-15 20:15:03 -0700

    create board with loops challenge 2

--- React/tictactoe/src/App.js
+++ React/tictactoe/src/App.js
@@ -9,6 +9,8 @@ function Square({ value, onSquareClick }) {
 }
 
 function Board({ xIsNext, squares, onPlay }) {
+  let board = [];
+
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
       return;
@@ -33,20 +35,22 @@ function Board({ xIsNext, squares, onPlay }) {
   return (
     <React.Fragment>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      <div>
+        {[0, 1, 2].map((row) => {
+          return (
+            <div key={row} className="board-row">
+              {[0, 1, 2].map((col) => {
+                let i = 3 * row + col;
+                return (
+                  <Square
+                    value={squares[i]}
+                    onSquareClick={() => handleClick(i)}
+                  />
+                );
+              })}
+            </div>
+          );
+        })}
       </div>
     </React.Fragment>
   );
