a7764f0eab6f572cecb6b3a35f731a599dc8bf55
Akash Jag <akashjagannathan408@gmail.com>
2024-04-27 03:00:52 -0700

    wasm interface bug

--- Analog/Quickstart/keygen.js
+++ Analog/Quickstart/keygen.js
@@ -1,18 +1,16 @@
-import { keygen } from "@analog-labs/timegraph-js/keygen/web";
-import { web3FromAddress } from "@polkadot/extension-dapp";
-// init signer based on polkadot/extension-dapp
-const { signer } = await web3FromAddress(
-  "5FHJQpmxMhopuWNCEQrHQJvzAe66nQkfT5zFWkaKYZHhvuRP"
-);
+import "dotenv/config.js";
+import Keyring from "@polkadot/keyring";
+import { keygen } from "@analog-labs/timegraph-js";
+
+// init polkadot keypair for node env
+const keyring = new Keyring({ type: "sr25519" });
+const keypair = keyring.addFromUri(process.env.PHRASE);
 // pass signer and address to get keygen instance
-const _keygen = new keygen({
-  signer: signer.signRaw,
-  address: "5FHJQpmxMhopuWNCEQrHQJvzAe66nQkfT5zFWkaKYZHhvuRP",
-});
-// generate api key i.e role is optional default will be developer
-const apikey = await _keygen.createApiKey();
-// generate the session key
-const sessionKey = await _keygen.createSessionkey();
+const _keygen = new keygen({ signer: keypair.sign, address: keypair.address });
+// generate API key, i.e. role is optional default will be developer
+const apiKey = await _keygen.createApiKey();
+// generate session key
+const sessionKey = await _keygen.createSessionkey("259200000");
 
 console.log("apiKey: ", apiKey);
 console.log("sessionKey: ", sessionKey);
