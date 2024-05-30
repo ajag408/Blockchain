ac562826b1e2fa3462d3f640cc64236170176235
Akash Jag <akashjagannathan408@gmail.com>
2024-05-29 23:25:50 -0700

    sepolia nft market

--- Alchemy/Road2Web3/7-NFTMarket/hardhat.config.js
+++ Alchemy/Road2Web3/7-NFTMarket/hardhat.config.js
@@ -2,7 +2,6 @@ require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 const fs = require("fs");
 // const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
-require("dotenv").config();
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -17,6 +16,14 @@ module.exports = {
   networks: {
     hardhat: {
       chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/nAhiCHKvZkhkp4A7PkkCIBON0-BXW26d`,
+      //accounts: [process.env.privateKey]
+    },
+    matic: {
+      url: "https://polygon-mainnet.g.alchemy.com/v2/nAhiCHKvZkhkp4A7PkkCIBON0-BXW26d",
+      //accounts: [process.env.privateKey]
     },
     sepolia: {
       url: process.env.REACT_APP_ALCHEMY_API_URL,
