d0ac34a358b1cfba07266ffb1f0ec548efd3679c
Akash Jag <akashjagannathan408@gmail.com>
2024-08-25 00:55:23 -0700

    van-life firestore

---
+++ React/Scrimba/AdvancedReact/van-life/api.js
@@ -0,0 +1,75 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDocs,
+  getDoc,
+} from "firebase/firestore/lite";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCOQ8-EBRAp5e5pkQybeEaYhZEPn0Hz9OU",
+  authDomain: "vanlife-3245d.firebaseapp.com",
+  projectId: "vanlife-3245d",
+  storageBucket: "vanlife-3245d.appspot.com",
+  messagingSenderId: "312783161563",
+  appId: "1:312783161563:web:2867bf7dd919175dc1e4fe",
+  measurementId: "G-23813TSWHJ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const vansCollectionRef = collection(db, "vans");
+
+export async function getVans() {
+  const snapshot = await getDocs(vansCollectionRef);
+  const vans = snapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+  return vans;
+}
+
+export async function getVan(id) {
+  const docRef = doc(db, "vans", id);
+  const snapshot = await getDoc(docRef);
+}
+
+export async function getHostVans(id) {
+  const url = id ? `/api/host/vans/${id}` : "/api/host/vans";
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw {
+      message: "Failed to fetch vans",
+      statusText: res.statusText,
+      status: res.status,
+    };
+  }
+  const data = await res.json();
+  return data.vans;
+}
+
+export async function loginUser(creds) {
+  const res = await fetch("/api/login", {
+    method: "post",
+    body: JSON.stringify(creds),
+  });
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw {
+      message: data.message,
+      statusText: res.statusText,
+      status: res.status,
+    };
+  }
+
+  return data;
+}
