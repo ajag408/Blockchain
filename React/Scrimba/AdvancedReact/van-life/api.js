2662cb7d5248c7992e5aaea9caa4d83e65d1a232
Akash Jag <akashjagannathan408@gmail.com>
2024-08-28 13:28:44 -0700

    van detail get van from firebase

--- React/Scrimba/AdvancedReact/van-life/api.js
+++ React/Scrimba/AdvancedReact/van-life/api.js
@@ -40,6 +40,10 @@ export async function getVans() {
 export async function getVan(id) {
   const docRef = doc(db, "vans", id);
   const snapshot = await getDoc(docRef);
+  return {
+    ...snapshot.data(),
+    id: snapshot.id,
+  };
 }
 
 export async function getHostVans(id) {
--- React/Scrimba/AdvancedReact/van-life/pages/Vans/VanDetail.jsx
+++ React/Scrimba/AdvancedReact/van-life/pages/Vans/VanDetail.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link, useParams, useLocation } from "react-router-dom"
-import { getVans } from "../../api"
+import { getVan } from "../../api"
 
 export default function VanDetail() {
     const [van, setVan] = React.useState(null)
@@ -13,7 +13,7 @@ export default function VanDetail() {
         async function loadVans() {
             setLoading(true)
             try {
-                const data = await getVans(id)
+                const data = await getVan(id)
                 setVan(data)
             } catch (err) {
                 setError(err)
