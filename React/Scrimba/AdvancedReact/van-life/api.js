5261dddf9823a91d731eeb5ec9d2b9147d1118cd
Akash Jag <akashjagannathan408@gmail.com>
2024-08-29 14:12:03 -0700

    van-life final

--- React/Scrimba/AdvancedReact/van-life/api.js
+++ React/Scrimba/AdvancedReact/van-life/api.js
@@ -6,6 +6,8 @@ import {
   doc,
   getDocs,
   getDoc,
+  query,
+  where,
 } from "firebase/firestore/lite";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -46,19 +48,29 @@ export async function getVan(id) {
   };
 }
 
-export async function getHostVans(id) {
-  const url = id ? `/api/host/vans/${id}` : "/api/host/vans";
-  const res = await fetch(url);
-  if (!res.ok) {
-    throw {
-      message: "Failed to fetch vans",
-      statusText: res.statusText,
-      status: res.status,
-    };
-  }
-  const data = await res.json();
-  return data.vans;
+export async function getHostVans() {
+  const q = query(vansCollectionRef, where("hostId", "==", "123"));
+  const snapshot = await getDocs(q);
+  const vans = snapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+  return vans;
 }
+
+// export async function getHostVans(id) {
+//     const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
+//     const res = await fetch(url)
+//     if (!res.ok) {
+//         throw {
+//             message: "Failed to fetch vans",
+//             statusText: res.statusText,
+//             status: res.status
+//         }
+//     }
+//     const data = await res.json()
+//     return data.vans
+// }
 
 export async function loginUser(creds) {
   const res = await fetch("/api/login", {
--- React/Scrimba/AdvancedReact/van-life/pages/Host/HostVanDetail.jsx
+++ React/Scrimba/AdvancedReact/van-life/pages/Host/HostVanDetail.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { useParams, Link, NavLink, Outlet } from "react-router-dom"
-import { getHostVans } from "../../api"
+import { getVan } from "../../api"
 
 export default function HostVanDetail() {
     const [currentVan, setCurrentVan] = React.useState(null)
@@ -12,7 +12,7 @@ export default function HostVanDetail() {
         async function loadVans() {
             setLoading(true)
             try {
-                const data = await getHostVans(id)
+                const data = await getVan(id)
                 setCurrentVan(data)
             } catch (err) {
                 setError(err)
