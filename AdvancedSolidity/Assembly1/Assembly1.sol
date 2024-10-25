83bd879ae436373d777889bac248f8fdeaca0a71
Akash Jag <akashjagannathan408@gmail.com>
2024-10-25 16:13:31 -0700

    assembly exercises 1 and 2

---
+++ AdvancedSolidity/Assembly1/Assembly1.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.4;
+
+contract Intro {
+    function intro() public pure returns (uint16) {
+        uint256 mol = 420;
+
+        // Yul assembly magic happens within assembly{} section
+        assembly {
+            // stack variables are instantiated with
+            // let variable_name := VALßUE
+            // instantiate a stack variable that holds the value of mol
+            // To return it needs to be stored in memory
+            // with command mstore(MEMORY_LOCATION, STACK_VARIABLE)
+            // to return you need to specify address and the size from the starting point
+            let val := mol
+            val := and(val, 0xffff)
+            let pts := mload(0x40)
+            mstore(pts, val)
+            return(pts, 0x20)
+        }
+    }
+}
